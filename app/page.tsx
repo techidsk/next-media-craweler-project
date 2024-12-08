fedd28288e9465e5f9709792ccd5df7f947822fc
techidsk <ecpknymt@gmail.com>
2024-12-08 17:03:02 +0800

    choro: mis spell

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,6 @@ export default function Home() {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        n
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
